b11d9776b94057ef9ba1fc23b1adca30d61fd6d1
safeervconn <muhammad.safeerullah@vconn.biz>
2025-06-20 12:16:42 +0500

    Updated Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -187,10 +187,10 @@ const Services = () => {
                               )}
                             </div>
                             <div className="flex-grow">
-                              <span className={`text-sm sm:text-base mobile-text  ${
+                              <span className={`text-sm sm:text-base mobile-text font-semibold ${
                                 feature.included 
                                   ? 'text-slate-800 dark:text-slate-100'
-                                  : 'text-black dark:text-black'
+                               //   : 'text-black dark:text-black'
                               }`}>
                                 {feature.name}
                               </span>
