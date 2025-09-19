29e208a61cb14b19d357b8347dabfd941bec56ba
safeervconn <muhammad.safeerullah@vconn.biz>
2025-09-19 12:41:18 +0500

    Updated test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = 'https://iutxllhudjckcaiwabud.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY; 
 
 
 const supabase = createClient(supabaseUrl, supabaseKey);
