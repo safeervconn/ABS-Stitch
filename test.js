541059fa268ec247003d501fa80199262c699be6
safeervconn <muhammad.safeerullah@vconn.biz>
2025-09-19 12:38:58 +0500

    Updated test.js

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-import "dotenv/config";
 
 const supabaseUrl = process.env.VITE_SUPABASE_URL;
 const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY; // anon key
