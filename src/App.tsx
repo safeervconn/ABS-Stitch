f53816fa6cb9305a5b0c788097d0f8c6eebb26a6
safeervconn <muhammad.safeerullah@vconn.biz>
2025-09-30 19:06:10 +0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './contexts/CartContext';
 import { OrderProvider } from './contexts/OrderContext';
-import { Bolt Database, signIn, getProducts } from './lib/Bolt Database';
-
 
 // Components
 import Navbar from './components/Navbar';
