ea0df68383180aeef84a0f0fe3b049fd65610128
safeervconn <muhammad.safeerullah@vconn.biz>
2025-10-02 18:12:05 +0500

    Updated CustomerOverviewTab.tsx

--- src/customer/tabs/CustomerOverviewTab.tsx
+++ src/customer/tabs/CustomerOverviewTab.tsx
@@ -147,9 +147,11 @@ const CustomerOverviewTab: React.FC = () => {
                 >
                   Place Your First Order
                 </button>
+              )}
             </div>
           )}
         </div>
+      </div>
     </div>
   );
 };
