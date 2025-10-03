50dd98f7566e610750530643598052a1fe6cfc7b
safeervconn <muhammad.safeerullah@vconn.biz>
2025-10-03 12:21:36 +0500

    Updated FilterBar.tsx

--- src/admin/components/FilterBar.tsx
+++ src/admin/components/FilterBar.tsx
@@ -107,7 +107,6 @@ const MultiSelectDropdown: React.FC<{
   }
   className="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
 />
-
                     <span className="text-sm text-gray-700">{option.label}</span>
                   </label>
                   
