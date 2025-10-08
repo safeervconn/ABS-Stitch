2569d957eba161dd5b5c3ad03d4f68ddcbfadd4a
safeervconn <muhammad.safeerullah@vconn.biz>
2025-10-08 20:30:13 +0500

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html', 
+    './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
     './src/customer/**/*.{js,ts,jsx,tsx}',
     './src/admin/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', '-apple-system', 'sans-serif'],
+      },
+      fontWeight: {
+        normal: '500',
+        medium: '600',
+        semibold: '700',
+        bold: '800',
+      },
+    },
   },
   plugins: [],
 };
