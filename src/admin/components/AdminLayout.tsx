31ec5c06218ade841804c383d6f1f9249a63f6eb
safeervconn <muhammad.safeerullah@vconn.biz>
2025-10-21 16:31:17 +0500

    Updated AdminLayout.tsx

--- src/admin/components/AdminLayout.tsx
+++ src/admin/components/AdminLayout.tsx
@@ -44,7 +44,7 @@ const AdminLayout: React.FC<AdminLayoutProps> = ({ children, activeTab, onTabCha
     { id: 'employees', label: 'Employees', icon: Users },
     { id: 'customers', label: 'Customers', icon: Users },
     { id: 'orders', label: 'Orders', icon: ShoppingBag },
-    { id: 'products', label: 'Products', icon: Package },
+    { id: 'stockdesigns', label: 'Products', icon: Package },
     { id: 'invoices', label: 'Invoices', icon: FileText },
   ];
 
