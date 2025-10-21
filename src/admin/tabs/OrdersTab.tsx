6e235d4175ee395367b1ba12ac44991ebca7457b
safeervconn <muhammad.safeerullah@vconn.biz>
2025-10-21 11:27:54 +0500

    Updated OrdersTab.tsx

--- src/admin/tabs/OrdersTab.tsx
+++ src/admin/tabs/OrdersTab.tsx
@@ -352,7 +352,7 @@ const OrdersTab: React.FC<OrdersTabProps> = ({ onOrderClick }) => {
         loading={loading}
       />
 
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
         {/* Order Edit Modal */}
         <EditOrderModal
         isOpen={isModalOpen}
