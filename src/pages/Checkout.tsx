15d685b8d333d6c44a4898db5407cd4cd8546141
safeervconn <muhammad.safeerullah@vconn.biz>
2025-10-22 14:13:27 +0500

    Updated Checkout.tsx

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -100,7 +100,7 @@ const Checkout: React.FC = () => {
         const itemTotal = itemPrice; // Always quantity 1
 
         const orderData = {
-          order_type: 'catalog' as 'catalog',
+          order_type: 'stock_design' as 'Stock Design',
           order_name: item.title,
           stock_design_id: item.id,
           custom_description: `${item.title}`,
